142fe074a7b6239e057e032ad77f57416fb2b797
tranviettrung <viettrung2580@gmail.com>
2026-01-15 22:42:06 +0700

    pause project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,41 @@
-import OurServices from "@/components/home/OurServices";
-import View from "@/components/home/View";
-import Discover from "@/components/home/Discover";
-import { Experiences } from "@/components/home/Experiences";
-import { Hero } from "@/components/home/Hero";
-import { Services } from "@/components/home/Services";
-import Timeline from "@/components/home/Timeline";
-import SliderTour from "@/components/slider-tour";
+// import OurServices from "@/components/home/OurServices";
+// import Discover from "@/components/home/Discover";
+// import { Experiences } from "@/components/home/Experiences";
+// import { Hero } from "@/components/home/Hero";
+// import Timeline from "@/components/home/Timeline";
+// import SliderTour from "@/components/slider-tour";
 
-export default function Home() {
-  return (
-    <div>
-      <div>
-        <Hero />
-        {/* <Services /> */}
-        <OurServices />
-        {/* <div className=" flex justify-center w-full h-full ">
-          {" "}
-          <video
-            autoPlay
-            controls
-            muted
-            loop
-            className=" w-full h-full object-cover"
-          >
-            {" "}
-            <source src="/video.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-        </div> */}
-        <SliderTour />
-        {/* <View /> */}
+// export default function Home() {
+//   return (
+//     <div>
+//       <div>
+//         <Hero />
+//         {/* <Services /> */}
+//         <OurServices />
+//         {/* <div className=" flex justify-center w-full h-full ">
+//           {" "}
+//           <video
+//             autoPlay
+//             controls
+//             muted
+//             loop
+//             className=" w-full h-full object-cover"
+//           >
+//             {" "}
+//             <source src="/video.mp4" type="video/mp4" />
+//             Your browser does not support the video tag.
+//           </video>
+//         </div> */}
+//         <SliderTour />
+//         {/* <View /> */}
 
-        <Discover />
-        <Experiences />
-        <Timeline />
-      </div>
-    </div>
-  );
+//         <Discover />
+//         <Experiences />
+//         <Timeline />
+//       </div>
+//     </div>
+//   );
+// }
+export default function Page() {
+  return <h1>We’ll be back soon 🚧</h1>;
 }
